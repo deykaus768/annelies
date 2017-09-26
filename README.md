1f8fe623f2e708ef52503c03e3d63915ca7998c2
deykaus768 <dey.kaushik768@gmail.com>
2017-09-27 00:34:06 +0530

    gulp-load-plugins (Factory pattern) to load all the gulp plugins

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Code Analysis using gulp-jshint and gulp-jscs
 2. npm install --save-dev gulp-jscs jshint gulp-jshint (--save-dev for only dev dependencies)
 3. jshint reporter - for showing the errors in a proper manner 
 	- jshint-stylish  {verbose:true}
-	- fail - if jshint,jscs issues are not passed then we ant to fail the task
+	- fail - if jshint,jscs issues are not passed then want to fail the task
 4. gulp-print,gulp-if,yargs (getting user arguement) - for printing all the files which is touched during the task.
+5. gulp-load-plugins - npm install --save-dev gulp-load-plugins (Factory pattern approach of gulp to load all the gulp-plugins)	
+	- var $ = require('gulp-load-plugins');
+	- gulp-print ($.print)
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,7 @@
 
 var gulp = require('gulp');
 var chalk = require('chalk');
-var jscs = require('gulp-jscs');
-var jshint = require('gulp-jshint');
-var gulpPrint = require('gulp-print');
-var gulpIf = require('gulp-if');
+var $ = require('gulp-load-plugins')({lazy:true});
 var config = require('./gulp.config')();
 var args = require('yargs').argv;
 
@@ -13,11 +10,11 @@ gulp.task('vet', function () {
     log('Vetting all js files using jshint,jscs');
     return gulp
         .src(config.js)
-        .pipe(gulpIf(args.verbose,gulpPrint()))
-        .pipe(jscs())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish', { verbose: true }))
-        .pipe(jshint.reporter('fail'));
+        .pipe($.if(args.verbose,$.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
+        .pipe($.jshint.reporter('fail'));
 });
 
 function log(msg) {
